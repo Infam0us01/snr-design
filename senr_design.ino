988d1e734b9e729e30ed0573a6ae2daa775162e0
Infam0us01 <karownoah7@gmail.com>
2025-03-25 12:38:59 -0400

    Add files via upload

--- senr_design.ino
+++ senr_design.ino
@@ -7,23 +7,68 @@ float ref = 1;
 //Sensor Input
 #include "SparkFunLSM6DSO.h"
 #include "Wire.h"
-#include "PID_v1.h"
 //#include "SPI.h"
 
 LSM6DSO myIMU; //Default constructor is I2C, addr 0x6B
 
 //PID
-//Define Variables we'll be connecting to
-double Setpoint, Input, Output;
+double setpoint, input;
 
-//Specify the links and initial tuning parameters
-double Kp=2, Ki=5, Kd=1;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+// double count = 0; //set the counts of the encoder
+// double angle = 0;//set the angles
+// boolean A,B;
+// byte state, statep;
+
+double pwm = 9; // this is the PWM pin for the motor for how much we move it to correct for its error
+const int dir = 4; //DIR pin to control the direction of the motor (clockwise/counter-clockwise)
+
+double Kp = 0.32;
+double Ki = 0.1;
+double Kd = -0.3;
+
+float last_error = 0;
+float error = 0;
+float changeError = 0;
+float totalError = 0;
+float pidTerm = 0;
+float pidTerm_scaled = 0;// if the total gain we get is not in the PWM range we scale it down so that it's not bigger than |255|
+
+
+// void setup() {
+//   Serial.begin(9600);
+//   pinMode(2, INPUT);//encoder pins
+//   pinMode(3, INPUT);
+//   attachInterrupt(0,Achange,CHANGE); //interrupt pins for encoder
+//   attachInterrupt(1,Bchange,CHANGE); 
+//   pinMode(pwm, OUTPUT);
+//   pinMode(dir, OUTPUT);
+
+// }
+
+void Angle2Position(){
+
+}
+
+void PIDcalculation(){
+  //angle = (0.9 * count); //count to angle conversion
+  error = setpoint - input;
+  
+  changeError = error - last_error; // derivative term
+  totalError += error; //accumalate errors to find integral term
+  pidTerm = (Kp * error) + (Ki * totalError) + (Kd * changeError);//total gain
+  pidTerm = constrain(pidTerm, -255, 255);//constraining to appropriate value
+  pidTerm_scaled = abs(pidTerm);//make sure it's a positive value
+
+  last_error = error;
+}
 
 void setup() {
   // LSM6DSO
   Serial.begin(115200);
-  delay(500); 
+  delay(500); //Do I need this?
+  pinMode(A0, INPUT)
+  pinMode(A1, INPUT)
   
   Wire.begin();
   delay(10);
@@ -37,22 +82,18 @@ void setup() {
   if( myIMU.initialize(BASIC_SETTINGS) )
     Serial.println("Loaded Settings.");
 
-  //PID
-  //initialize the variables we're linked to
-  Input = analogRead(PIN_INPUT);
-  Setpoint = 100;
-
-  //turn the PID on
-  myPID.SetMode(AUTOMATIC);
+  //PID 
+  pinMode(pwm, OUTPUT);
+  pinMode(dir, OUTPUT);
 
 
 }
 
 void loop() {
- 
   A_r1 = (analogRead(analogIn))/1024;
   A_r2 = analogRead(analogIn2)/2048;
   ref_pos = A_r1 * sin(2*pi()*A_r2*millis()); //ref sine wave
+  setpoint = ref_pos;
 
   //LSM6DSO
   //Get all parameters
@@ -85,10 +126,27 @@ void loop() {
 
   //change in angular position
   float changeXAngle = changeXGyro * 1000;
+  input = changeXAngle;
 
-  myPID.Compute();
-  float changeYAngle = changeYGyro * 1000;
-  float changeZAngle = changeZGyro * 1000;
+  PIDcalculation();// find PID value
+  
+  if (pidTerm >0) {
+    digitalWrite(dir, HIGH);// Forward motion
+    
+  } else {
+    digitalWrite(dir, LOW);//Reverse motion
+    
+  }
+
+  analogWrite(pwm, pidTerm_scaled);
+
+  Serial.println(" ANGLE: ");
+  Serial.print(angle);
+
+  // delay(100);
+
+  // float changeYAngle = changeYGyro * 1000;
+  // float changeZAngle = changeZGyro * 1000;
 
   //print values
 
